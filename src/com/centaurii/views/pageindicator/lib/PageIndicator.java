891dd0694439e0e8712f093adb1c5aad5e2b7df3
Andrew Schuster <aschuster3@gatech.edu>
2014-06-09 13:34:38 -0400

    Initial Commit

---
+++ src/com/centaurii/views/pageindicator/lib/PageIndicator.java
@@ -0,0 +1,25 @@
+package com.centaurii.views.pageindicator.lib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PageIndicator extends View
+{
+
+    public PageIndicator(Context context)
+    {
+        super(context);
+    }
+    
+    public PageIndicator(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+    
+    public PageIndicator(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
